f1f3b8bc5c17415726b8d065601ab2fb1d84c999
AmirmahdiShayankhwah <amyrmhdyshaya7@gmail.com>
2023-07-23 10:29:52 +0430

    first aray

---
+++ arayy/arayy/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arayy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] student = new int[20];
+            student[0] = 14;
+            student[1] = 16;
+            student[2] = 18;
+            student[3] = 22;
+            for (int i = 0; i < 3; i++)
+
+                Console.WriteLine(student[i]);
+            
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
