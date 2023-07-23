467af158095885e8d3de48abaa9ef51ef83eea69
AmirmahdiShayankhwah <amyrmhdyshaya7@gmail.com>
2023-07-23 10:15:12 +0430

    first mosbat manfi

---
+++ test/test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = int.Parse(System.Console.ReadLine());
+            if (a > 1)
+                Console.WriteLine("mosbat");
+            else if (a < 0)
+                Console.WriteLine("manfi");
+
+            Console.ReadKey();
+
+        }
+    }
+}
